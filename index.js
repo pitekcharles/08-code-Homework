6c74d101cb9913d226a44437ec2fec05796e4441
unknown <charles.pitek@gmail.com>
2020-01-14 16:01:21 -0800

    called out packages created inquirer prompts, called out github api for user data

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+
+
+inquirer.prompt([
+    {
+        type: "input",
+        message: "What is the developers github profile?",
+        name : "username"
+
+    },
+    {
+        type: "list",
+        message: "What is your favorite color?",
+        name: "colorFav",
+        choices: [
+            "red",
+            "pink",
+            "blue",
+            "green"
+        ]
+    }
+]).then(function(data){
+    const {username, colorFav} = data;
+    const queryUrl = `https://api.github.com/users/${username}`;
+
+    axios.get(queryUrl).then(function(response){
+        // console.log(response.data);
+        const {avatar_url, name, location: locCurrent, url, bio, public_repos, followers, following} = response.data;
+    })
+})
+
+
+
+
 const questions = [
   
 ];
@@ -8,4 +44,6 @@ function writeToFile(fileName, data) {
 
 function init() {
 
-init();
+}
+
+// init();
